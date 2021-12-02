a0cf1bacc664da90f28a9b99109b957042f478ef
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-12-02 20:44:46 -0300

    Configurando a aplicacaoProducer

--- src/main/java/expertostech/kafka/producer/AplicacaoProducer.java
+++ src/main/java/expertostech/kafka/producer/AplicacaoProducer.java
@@ -1,12 +1,18 @@
 package expertostech.kafka.producer;
 
+import expertostech.kafka.eventos.ProdutorEvento;
+
 public class AplicacaoProducer {
     public static void main(String[] args) {
+        AplicacaoProducer aplicacao = new AplicacaoProducer();
+        aplicacao.iniciar();
 
     }
 
     //Método que vai iniciar o nosso programa
     private void iniciar(){
+        ProdutorEvento produtor = new ProdutorEvento();
+        produtor.executar();
 
     }
 }
